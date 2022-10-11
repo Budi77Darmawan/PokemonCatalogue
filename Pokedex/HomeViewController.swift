42d7665cb6f596e462f2cfe12035c383ac17fb03
Budi77Darmawan <budymitman_77@yahoo.co.id>
2022-10-11 21:06:34 +0700

    initial project

--- Pokedex/HomeViewController.swift
+++ Pokedex/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Pokedex
 //
 //  Created by Budi Darmawan on 11/10/22.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
 }
-
